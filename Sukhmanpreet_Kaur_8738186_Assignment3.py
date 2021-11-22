6f4e372886fc40798b8c1123013a9438353a68d4
sukh1401 <Skaur8186@coenstogac.on.ca>
2021-11-22 22:22:50 +0300

    Factorial Program

--- Sukhmanpreet_Kaur_8738186_Assignment3.py
+++ Sukhmanpreet_Kaur_8738186_Assignment3.py
@@ -1,40 +1,137 @@
+from datetime import datetime
 
-class BankSystem:
-    balance = 0
 
-    def __init__(self):
-        amount = float(input("Enter initial deposit amount: "))
-        self.balance = amount
-        self.displayBalance()
-        self.runUserTasks()
+class PoloShirts():
+    name = 'Polo Shirt'
+    quantity = 0
+    color = ''
 
-    def runUserTasks(self):
-        operation = input("Enter operation to perform:(deposit/withdraw) ")
-        if(operation == "deposit"):
-            self.deposit()
-        elif(operation == "withdraw"):
-            self.withdraw()
+    def __init__(self,  color, quantity):
+        self.quantity = quantity
+        self.color = color
+
+    def __str__(self):
+        return self.name
+
+
+class TShirts():
+    name = 'T-Shirt'
+    quantity = 0
+    color = ''
+
+    def __init__(self,  color, quantity):
+        self.quantity = quantity
+        self.color = color
+
+    def __str__(self):
+        return self.name
+
+
+class Calculate:
+    def calcTotal(self, quantity, price):
+        return round((quantity * price), 2)
+
+    def calcHST(self, totalCost):
+        return round(((13/100) * totalCost), 2)
+
+    def calTotalPayable(self, totalCost, hst):
+        return round((totalCost + hst), 2)
+
+
+class OrderSystem:
+    company = "=====    Abby's Merchandizing (AM)    ======"
+    welcomeMessage = "== Welcome to our Online Shirt Orders Shop!!! =="
+    dateOfOrder = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    totalCost = 0
+    totalPayable = 0
+    CONSTANT_PRICE = 9.99
+    product = None
+
+    def run(self):
+        print(self.company)
+        print(self.welcomeMessage)
+        self.getUserInputs()
+        calculator = Calculate()
+        self.totalCost = calculator.calcTotal(
+            self.product.quantity, self.CONSTANT_PRICE)
+        self.hst = calculator.calcHST(self.totalCost)
+        self.totalPayable = calculator.calTotalPayable(
+            self.totalCost, self.hst)
+        self.printReceiptSummary()
+
+    def getUserInputs(self):
+        color = input("Which color of shirt do you want to order: ")
+        typeOfShirt = int(
+            input("Which shirt do you want? (Enter 1 for Polo, 2 for Shirt): "))
+
+        quantity = int(input("How many shirts do you want?: "))
+
+        if(typeOfShirt == 1):
+            self.product = PoloShirts(color, quantity)
+        elif typeOfShirt == 2:
+            self.product = TShirts(color, quantity)
         else:
-            print("Invalid operation")
-        self.displayBalance()
+            print("Please enter a valid type of shirt option:")
+            quantity = 0
+            color = None
+            typeOfShirt = None
+            self.getUserInputs()
+        if quantity < 1:
+            print("Quantity should be greater than zero")
+            quantity = 0
+            color = None
+            typeOfShirt = None
+            self.getUserInputs()
 
-    def deposit(self):
-        amount = float(input("Enter amount to be deposited: $ "))
-        self.balance += amount
+    def printReceiptSummary(self):
 
-    def withdraw(self):
-        amount = float(input("Enter amount to be withdrawn: $"))
-        if(self.balance >= amount):
-            self.balance -= amount
-            print("You withdrew $%.2f from the account" % amount)
-        else:
-            print("Insufficient funds")
+        summary = f"""
+                      Receipt       {self.dateOfOrder}
+                _________________________________
+                    {self.company}
+            ______________________________________________________________
+            Description                 Qty          Price            Total
+            ______________________________________________________________
+            {self.product.color} {self.product.name}                 {self.product.quantity}     x    ${self.CONSTANT_PRICE}        = ${self.totalCost}
+            ______________________________________________________________
+                TOTAL AMOUNT                                      ${self.totalPayable}
 
-    def displayBalance(self):
-        print("Available balance: $%.2f" % self.balance)
+                Sub Total                                         ${self.totalCost}
+                Harmonized Sales Tax                              ${self.hst}
+                Balance                                           ${self.totalPayable}
 
-if __name__ == '__main__':    
-    # Bank system
-    print(" Bank system")
-    bankSystem = BankSystem()
-    print("====================================")
+                ||||||||||||||||||||||||||||||||
+            """
+        print(summary)
+
+class Factorial:
+    def factorial(self, value):
+
+        product = 1
+        for i in range(1, value+1):
+            product *= i
+        return product
+
+    def getUserInputs(self):
+        value = int(input("Enter a number: "))
+        return value
+
+    def run(self):
+        number = self.getUserInputs()
+        result = self.factorial(number)
+        print(f"The factorial of {number} is {result}")
+
+
+
+
+
+if __name__ == '__main__':
+    # Order System
+    orderSystem = OrderSystem()
+    orderSystem.run()
+
+    # Factorial Program
+    factorialCalculator = Factorial()
+    factorialCalculator.run()
+
+   
